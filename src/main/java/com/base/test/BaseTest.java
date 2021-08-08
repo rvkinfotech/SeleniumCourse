954d1e915ec4386f201ae8c1083f3c1845e48880
user <user@DESKTOP-TC2LUN4>
2021-08-08 16:53:16 -0700

    update dynamic path for drivers and parameter added in testng.xml

--- src/main/java/com/base/test/BaseTest.java
+++ src/main/java/com/base/test/BaseTest.java
@@ -14,17 +14,17 @@ public class BaseTest{
 		
 		if(browserName.equals("Chrome")) {
 			
-			  System.setProperty("webdriver.chrome.driver", "C:\\Users\\user\\eclipse-workspace\\demo\\Drivers\\chrome\\chromedriver.exe");
+			  System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"\\Drivers\\chrome\\chromedriver.exe");
 			  webDriver=new ChromeDriver();
 			
 		}else if(browserName.equals("Firefox")){
 			 
-			System.setProperty("webdriver.gecko.driver","C:\\Users\\user\\eclipse-workspace\\demo\\Drivers\\gecko\\geckodriver.exe");
+			System.setProperty("webdriver.gecko.driver",System.getProperty("user.dir")+"\\Drivers\\gecko\\geckodriver.exe");
 			webDriver=new FirefoxDriver();
 			
 		}else if(browserName.equals("Edge")) {
 			
-			System.setProperty("webdriver.edge.driver","C:\\Users\\user\\eclipse-workspace\\demo\\Drivers\\edge\\msedgedriver.exe");
+			System.setProperty("webdriver.edge.driver",System.getProperty("user.dir")+"\\Drivers\\edge\\msedgedriver.exe");
 			webDriver=new EdgeDriver();
 			 
 			
