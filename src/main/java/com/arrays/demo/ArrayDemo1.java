3faf7f5c6cc66b220d20de9d8db647d1d5c18cbe
user <user@DESKTOP-TC2LUN4>
2021-08-12 21:38:41 -0700

    missing file

--- src/main/java/com/arrays/demo/ArrayDemo1.java
+++ src/main/java/com/arrays/demo/ArrayDemo1.java
@@ -10,9 +10,9 @@ public class ArrayDemo1 {
 		
 		for(int i=0;i<a.length;i++) {
 			
-			//System.out.println("a["+i+"]="+a[i]);
+			System.out.println("a["+i+"]="+a[i]);
 			
-			System.out.print(a[i]);
+			//System.out.print(a[i]);
 		}
 		System.out.println();
 		System.out.println("Now in Reverse");
